65b99bdd808d4bac4d33a9ce50d7a00df906647a
Afigodoga <godogawoedem5@gmail.com>
2024-07-01 12:59:24 +0000

    The specifications of the phones

--- test.py
+++ test.py
@@ -3,3 +3,11 @@ print('hello world')
 print('glad to be at work today')
 Price=900
 print('The price of the iphone is $',Price,sep='')
+Price=900,600,899,1000
+print('The prices of the phones are',Price,sept='')
+Brand=('iphone','sumsung','LG','iphone')
+print('The brand of the phones are',Brand)
+ram=12,32,64,128
+print('The ram of the phones are',Brand('in GB'))
+Storage= 32,64,128,128
+print('The storage of the phones are',Storage('in GB'))
