1310ae35a0bef9b6b6d1c901d28cf4d3d944e60d
Afigodoga <godogawoedem5@gmail.com>
2024-07-02 17:12:44 +0000

    First Project

--- test.py
+++ test.py
@@ -1,32 +1,32 @@
-print('hello world')
+# print('hello world')
 
-print('glad to be at work today')
-Price=900
-print('The price of the iphone is $',Price,sep='')
-Price=900,600,899,1000
-print('The prices of the phones are',Price,sep='')
-Brand=('iphone','sumsung','LG','iphone')
-print('The brand of the phones are',Brand,)
-ram=12,32,64,128
-print('The ram of the phones are',ram,('in GB'))
-Storage= 32,64,128,128
-print('The storage of the phones are',Storage,('in GB'))
+# print('glad to be at work today')
+# Price=900
+# print('The price of the iphone is $',Price,sep='')
+# Price=900,600,899,1000
+# print('The prices of the phones are',Price,sep='')
+# Brand=('iphone','sumsung','LG','iphone')
+# print('The brand of the phones are',Brand,)
+# ram=12,32,64,128
+# print('The ram of the phones are',ram,('in GB'))
+# Storage= 32,64,128,128
+# print('The storage of the phones are',Storage,('in GB'))
 
-Budget=int(input('Enter your budget(in dollars):'))
-if Price<=Budget:
-    print('Congratulation! you can buy the phone')
-else:
-    print('sorry your budget is not enough to buy the phone')
+# Budget=int(input('Enter your budget(in dollars):'))
+# if Price<=Budget:
+#     print('Congratulation! you can buy the phone')
+# else:
+#     print('sorry your budget is not enough to buy the phone')
 
-    memory = int(input('Enter the memory'))
-    if memory==32:
-     print('The price of the phone is $900')
-    elif memory==64:
-     print('The price of the phone is $600')
-    else:
-       print('phone is unavailable')
-       color=('red','blue','orange','pink')
-       print('The color of the phone is',color,)
+#     memory = int(input('Enter the memory'))
+#     if memory==32:
+#      print('The price of the phone is $900')
+#     elif memory==64:
+#      print('The price of the phone is $600')
+#     else:
+#        print('phone is unavailable')
+#        color=('red','blue','orange','pink')
+#        print('The color of the phone is',color,)
 
 
-
+import pandas as pd
